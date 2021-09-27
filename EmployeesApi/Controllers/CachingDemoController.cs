e010b4fcd201ab971f764726ea236b268c0a1d87
ginorosati21 <ginorosati21@gmail.com>
2021-09-27 14:17:46 -0400

    Initial commit

---
+++ EmployeesApi/Controllers/CachingDemoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Controllers
+{
+    public class CachingDemoController : ControllerBase
+    {
+
+        [HttpGet("/cached")]
+        public async Task<ActionResult> GetSomeSlowStuff()
+        {
+            await Task.Delay(3000);
+            return Ok(new { data = "Your Stuff Here" });
+        }
+    }
+}
